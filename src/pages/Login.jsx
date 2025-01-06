0a9c838467dbc4565691bff7a3659f818a1c3620
charmdude10 <abioyemichael4@gmail.com>
2025-01-06 14:03:50 +0100

    my first commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// import { FcGoogle } from "react-icons/fc";
+// import { FaApple } from "react-icons/fa6";
+
+const Login = () => {
+  return (
+    <div
+      className="flex justify-center items-center h-[80vh]"
+      style={{
+        backgroundImage: "url('/img/illustration-flying-airplane.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md h-[full]">
+        <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
+        <form>
+          <div className="mb-4">
+            <label className="block mb-2 text-sm font-medium text-gray-700">
+              Enter Email Address
+            </label>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block mb-2 text-sm font-medium text-gray-700">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Enter your password"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600"
+          >
+            Login
+          </button>
+        </form>
+
+        <div className="mt-6 text-center">
+          <p className="text-sm text-gray-500 mb-2">Or Login With</p>
+          <div className="flex justify-center space-x-4">
+            <a
+              href="#"
+              className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition duration-300"
+            >
+              {/* <FcGoogle /> */}
+              Google
+            </a>
+            <a
+              href="#"
+              className="px-4 py-2 bg-black text-white rounded-lg hover:bg-gray-800 transition duration-300"
+            >
+              {/* <FaApple /> */}
+              Apple
+            </a>
+          </div>
+        </div>
+
+        <p className="mt-4 text-center text-sm">
+          Don’t have an account?{" "}
+          <Link
+            to="/signup"
+            className="text-blue-500 hover:underline font-bold"
+          >
+            Sign Up
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Signup = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+        <h2 className="text-2xl font-bold text-center mb-6">Create Account</h2>
+        <form>
+          <div className="mb-4">
+            <label className="block mb-2 text-sm font-medium text-gray-700">
+              Enter Full Name
+            </label>
+            <input
+              type="text"
+              placeholder="Enter your full name"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block mb-2 text-sm font-medium text-gray-700">
+              Enter Email Address
+            </label>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block mb-2 text-sm font-medium text-gray-700">
+              Enter Password
+            </label>
+            <input
+              type="password"
+              placeholder="Create a password"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-green-500 text-white py-2 rounded-lg hover:bg-green-600"
+          >
+            Sign Up
+          </button>
+        </form>
+
+        <div className="mt-6 text-center">
+          <p className="text-sm text-gray-500 mb-2">Or Sign Up With</p>
+          <div className="flex justify-center space-x-4">
+            <a
+              href="#"
+              className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition duration-300"
+            >
+              Google
+            </a>
+            <a
+              href="#"
+              className="px-4 py-2 bg-black text-white rounded-lg hover:bg-gray-800 transition duration-300"
+            >
+              Apple
+            </a>
+          </div>
+        </div>
+        <p className="mt-4 text-center text-sm">
+          Already have an account?{" "}
+          <Link to="/login" className="text-green-500 hover:underline">
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
